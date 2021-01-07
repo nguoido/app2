b9330dc7b0c7c133c8e9867bc2eb13d116842da3
Do Khai <Khai.Do@windriver.com>
2021-01-07 15:50:50 +0700

    intent receive

--- app/src/main/java/com/dkhai/app2/MainActivity.java
+++ app/src/main/java/com/dkhai/app2/MainActivity.java
@@ -2,6 +2,7 @@ package com.dkhai.app2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -11,36 +12,56 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("lifecycle","onCreate invoked");
+        Log.d("lifecycle", "onCreate invoked");
     }
+
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d("lifecycle","onStart invoked");
+        Log.d("dkhai", "onStart invoked");
+
+        // Bundle extras = getIntent().getExtras();
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        if (extras == null) {
+            Log.d("dkhai", "extras = null");
+        } else {
+            String qString = extras.getString("qString");
+            Log.d("dkhai", qString);
+        }
     }
+
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d("lifecycle","onResume invoked");
+        Log.d("dkhai", "onResume invoked");
     }
+
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d("lifecycle","onPause invoked");
+        Log.d("dkhai", "onPause invoked");
     }
+
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d("lifecycle","onStop invoked");
+        Log.d("dkhai", "onStop invoked");
+
+        Intent intent = getIntent();
+        intent.removeExtra("triggerBRC");
     }
+
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.d("lifecycle","onRestart invoked");
+        Log.d("dkhai", "onRestart invoked");
     }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d("lifecycle","onDestroy invoked");
+        Log.d("dkhai", "onDestroy invoked");
     }
+    
 }
